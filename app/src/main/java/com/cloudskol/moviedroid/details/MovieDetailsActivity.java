1ffafe816f4cc6b43287ec94e2873acbddb645da
Thamizharasu <zarub2k@gmail.com>
2015-12-15 22:59:47 +0530

    Un-used files are removed

--- app/src/main/java/com/cloudskol/moviedroid/details/MovieDetailsActivity.java
+++ app/src/main/java/com/cloudskol/moviedroid/details/MovieDetailsActivity.java
@@ -22,7 +22,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_details);
+        setContentView(R.layout.movie_details_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
--- app/src/main/java/com/cloudskol/moviedroid/list/MovieListActivity.java
+++ app/src/main/java/com/cloudskol/moviedroid/list/MovieListActivity.java
@@ -37,7 +37,7 @@ public class MovieListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spotify);
+        setContentView(R.layout.movie_list_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
