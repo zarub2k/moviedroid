5be08078519b30446d6ec54c10295ac7b63e742a
Thamizharasu <zarub2k@gmail.com>
2015-12-24 00:37:38 +0530

    Code added for playing Youtube videos

--- app/src/main/java/com/cloudskol/moviedroid/details/DetailFragment.java
+++ app/src/main/java/com/cloudskol/moviedroid/details/DetailFragment.java
@@ -1,12 +1,15 @@
 package com.cloudskol.moviedroid.details;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RatingBar;
@@ -65,13 +68,33 @@ public class DetailFragment extends Fragment {
     }
 
     private void renderTrailer(View trailerView, int movieId) {
-        final ListView trailerListView = (ListView) trailerView.findViewById(R.id.trailer_listview);
-
         trailerListAdapter = new TrailerListAdapter(getContext(), new Trailer());
+        final ListView trailerListView = (ListView) trailerView.findViewById(R.id.trailer_listview);
         trailerListView.setAdapter(trailerListAdapter);
+
+        //Adding click event for the list view
+        trailerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final Video video = trailerListAdapter.getItem(position);
+                watchYoutubeVideo(video.getKey());
+            }
+        });
 
         final TrailerTask trailerTask = new TrailerTask(this);
         trailerTask.execute(moviedroidUriBuilder.getTrailer(movieId));
+    }
+
+    private void watchYoutubeVideo(String id) {
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + id));
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Log.e(LOG_TAG, "Error while playing video with Youtube", e);
+            Intent intent=new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://www.youtube.com/watch?v="+id));
+            startActivity(intent);
+        }
     }
 
     private void renderMovieDetails(int movieId) {
