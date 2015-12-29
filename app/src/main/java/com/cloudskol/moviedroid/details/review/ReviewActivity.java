00e28f909e5cd9c8fe3c975dbd92ec4032a066f4
Thamizharasu <zarub2k@gmail.com>
2015-12-30 00:02:22 +0530

    List adapter imply started for review

--- app/src/main/java/com/cloudskol/moviedroid/details/review/ReviewActivity.java
+++ app/src/main/java/com/cloudskol/moviedroid/details/review/ReviewActivity.java
@@ -6,6 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import com.cloudskol.moviedroid.R;
+import com.cloudskol.moviedroid.common.MoviedroidPropertyReader;
+import com.cloudskol.moviedroid.common.MoviedroidUriBuilder;
+import com.cloudskol.moviedroid.model.Review;
+
+import java.util.List;
 
 /**
  * @author tham
@@ -16,11 +21,16 @@ public class ReviewActivity extends AppCompatActivity {
     private static final String LOG_TAG = ReviewActivity.class.getSimpleName();
 
     private ReviewListAdapter reviewListAdapter;
+    private MoviedroidPropertyReader moviedroidPropertyReader;
+    private MoviedroidUriBuilder moviedroidUriBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.review_content);
+
+        moviedroidPropertyReader = MoviedroidPropertyReader.getInstance(this);
+        moviedroidUriBuilder = new MoviedroidUriBuilder(moviedroidPropertyReader);
 
         renderReview(getIntent().getIntExtra(Intent.EXTRA_TEXT, -1));
     }
@@ -29,5 +39,12 @@ public class ReviewActivity extends AppCompatActivity {
         reviewListAdapter = new ReviewListAdapter();
         ListView reviewListView = (ListView) findViewById(R.id.review_listview);
         reviewListView.setAdapter(reviewListAdapter);
+
+        final ReviewTask reviewTask = new ReviewTask(this);
+        reviewTask.execute(moviedroidUriBuilder.getReviews(movieId));
+    }
+
+    public void onReviewDataReceived(List<Review> reviews) {
+        reviewListAdapter.setReviews(reviews);
     }
 }
--- app/src/main/java/com/cloudskol/moviedroid/details/review/ReviewListAdapter.java
+++ app/src/main/java/com/cloudskol/moviedroid/details/review/ReviewListAdapter.java
@@ -1,14 +1,27 @@
 package com.cloudskol.moviedroid.details.review;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import com.cloudskol.moviedroid.model.Review;
+
+import java.util.List;
 
 /**
  * @author tham
  */
 public class ReviewListAdapter extends BaseAdapter {
     private static final String LOG_TAG = ReviewListAdapter.class.getSimpleName();
+
+    private List<Review> reviews;
+    final LayoutInflater layoutInflater;
+
+    public ReviewListAdapter(ReviewActivity reviewActivity, List<Review> reviews) {
+        this.reviews = reviews;
+        layoutInflater = reviewActivity.getLayoutInflater();
+    }
 
     @Override
     public int getCount() {
@@ -28,5 +41,9 @@ public class ReviewListAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         return null;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        notifyDataSetChanged();
     }
 }
