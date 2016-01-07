54ee481b436f435be942a7853561a6fc119358e8
Thamizharasu <zarub2k@gmail.com>
2016-01-07 23:00:32 +0530

    Favourite menu item added

--- app/src/main/java/com/cloudskol/moviedroid/provider/MovieContract.java
+++ app/src/main/java/com/cloudskol/moviedroid/provider/MovieContract.java
@@ -25,7 +25,6 @@ public class MovieContract {
 
         public static final String TABLE_NAME = "movie";
 
-        public static final String COLUMN_MOVIE_ID = "movie_id";
         public static final String COLUMN_TITLE = "title";
         public static final String COLUMN_OVERVIEW = "overview";
         public static final String COLUMN_RATING = "rating";
