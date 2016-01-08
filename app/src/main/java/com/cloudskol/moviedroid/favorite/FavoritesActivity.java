2bd58b76f6b7191c68df8a5c9bc5780ffcc365eb
Thamizharasu <zarub2k@gmail.com>
2016-01-09 00:06:10 +0530

    Favourite activity introduced

---
+++ app/src/main/java/com/cloudskol/moviedroid/favorite/FavoritesActivity.java
@@ -0,0 +1,16 @@
+package com.cloudskol.moviedroid.favorite;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cloudskol.moviedroid.R;
+
+/**
+ * @author tham
+ */
+public class FavoritesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
