13cb221bd35934638c34741c1d934a3e65e57274
Thamizharasu <zarub2k@gmail.com>
2016-01-11 00:04:55 +0530

    Enhancement done for favourite movies

--- app/src/main/java/com/cloudskol/moviedroid/favorite/FavoritesActivity.java
+++ app/src/main/java/com/cloudskol/moviedroid/favorite/FavoritesActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.cloudskol.moviedroid.R;
+import com.cloudskol.moviedroid.model.Movie;
 import com.cloudskol.moviedroid.provider.MovieDbHelper;
+
+import java.util.List;
 
 /**
  * @author tham
@@ -25,5 +28,9 @@ public class FavoritesActivity extends AppCompatActivity {
         final MovieDbHelper movieDbHelper = new MovieDbHelper(this);
         final FavoritesTask favoritesTask = new FavoritesTask(this, movieDbHelper);
         favoritesTask.execute();
+    }
+
+    public void onFavoriteMoviesReceived(List<Movie> movies) {
+        
     }
 }
--- app/src/main/java/com/cloudskol/moviedroid/favorite/FavoritesTask.java
+++ app/src/main/java/com/cloudskol/moviedroid/favorite/FavoritesTask.java
@@ -10,6 +10,7 @@ import com.cloudskol.moviedroid.model.Movie;
 import com.cloudskol.moviedroid.provider.MovieContract;
 import com.cloudskol.moviedroid.provider.MovieDbHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,10 +20,10 @@ public class FavoritesTask extends AsyncTask<Void, Void, List<Movie>> {
 
     private static final String LOG_TAG = FavoritesTask.class.getSimpleName();
 
-    private Context context_;
+    private FavoritesActivity context_;
     private MovieDbHelper dbHelper_;
 
-    public FavoritesTask(Context context, MovieDbHelper dbHelper) {
+    public FavoritesTask(FavoritesActivity context, MovieDbHelper dbHelper) {
         context_ = context;
         dbHelper_ = dbHelper;
     }
@@ -33,10 +34,41 @@ public class FavoritesTask extends AsyncTask<Void, Void, List<Movie>> {
         return null;
     }
 
-    private void getFavoriteMovies() {
+    private List<Movie> getFavoriteMovies() {
         final SQLiteDatabase database = dbHelper_.getReadableDatabase();
         final Cursor cursor = database.query(MovieContract.MovieEntry.TABLE_NAME,
                 null, null, null, null, null, null);
-        Log.v(LOG_TAG, "Size of the cursor is: " + cursor.getCount());
+        final int count = cursor.getCount();
+        Log.v(LOG_TAG, "Size of the cursor is: " + count);
+
+        if (count == 0) {
+            return null;
+        }
+
+        cursor.moveToFirst();
+        List<Movie> favoriteMovies = new ArrayList<Movie>(count);
+        for (int i = 0; i < count; i++) {
+            favoriteMovies.add(getMovie(cursor));
+            cursor.moveToNext();
+        }
+
+        return favoriteMovies;
+    }
+
+    @Override
+    protected void onPostExecute(List<Movie> movies) {
+        context_.onFavoriteMoviesReceived(movies);
+    }
+
+    private Movie getMovie(Cursor cursor) {
+        final long id = cursor.getLong(cursor.getColumnIndexOrThrow(
+                MovieContract.MovieEntry._ID));
+        final String title = cursor.getString(cursor.getColumnIndexOrThrow(
+                MovieContract.MovieEntry.COLUMN_TITLE));
+
+        Log.v(LOG_TAG, title + " > " + id);
+
+        final Movie movie = new Movie((int) id, title);
+        return movie;
     }
 }
