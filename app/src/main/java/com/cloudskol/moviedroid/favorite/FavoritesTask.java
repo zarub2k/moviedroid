2caeee59af470c10b302bc2e36146b1b3c9e48e2
Thamizharasu <zarub2k@gmail.com>
2016-01-13 22:46:31 +0530

    un wanted imports removed

--- app/src/main/java/com/cloudskol/moviedroid/favorite/FavoritesTask.java
+++ app/src/main/java/com/cloudskol/moviedroid/favorite/FavoritesTask.java
@@ -1,6 +1,5 @@
 package com.cloudskol.moviedroid.favorite;
 
-import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
