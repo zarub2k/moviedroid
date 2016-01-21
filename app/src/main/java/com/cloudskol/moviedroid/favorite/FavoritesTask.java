5182df4b833ea63393c02944e7f5e00bf0fabf99
Thamizharasu <zarub2k@gmail.com>
2016-01-21 22:17:17 +0530

    Javadoc is added

--- app/src/main/java/com/cloudskol/moviedroid/favorite/FavoritesTask.java
+++ app/src/main/java/com/cloudskol/moviedroid/favorite/FavoritesTask.java
@@ -18,6 +18,8 @@ import java.util.List;
 
 /**
  * @author tham
+ * 
+ * Async tasks to fetch all the favorites movies from db
  */
 public class FavoritesTask extends AsyncTask<Void, Void, Cursor> {
 
